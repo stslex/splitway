3a60933c1eb01865fb173883fe9fd7343a4222b3
stslex <ilya977.077@gmail.com>
2026-04-01 21:48:29 +0300

    init basic daemon

--- splitway-daemon/src/main.rs
+++ splitway-daemon/src/main.rs
@@ -1,3 +1,81 @@
+use std::process::{exit, Command};
+
+use splitway_shared::config::{get_config, ConfigParseError};
+
+use crate::config::{ConfigController, ResolvedConfig};
+
+mod config;
+
 fn main() {
-    println!("init daemon");
+    env_logger::init();
+    let config = get_resolved_config();
+
+    log::info!("Resolved config: {:?}", config);
+
+    call_resolvectl(&config);
+    add_dns_domain(&config);
+}
+
+fn get_resolved_config() -> ResolvedConfig {
+    match get_config() {
+        Ok(config) => config,
+        Err(e) => {
+            match e {
+                ConfigParseError::ConfigNotFound => {
+                    log::error!("Config file not found, creating empty config");
+                    if let Err(e) = splitway_shared::config::create_empty_config() {
+                        log::error!("Error create empty config: {e}");
+                    }
+                }
+                _ => log::error!("Error get config: {e}"),
+            }
+            exit(1);
+        }
+    }
+    .resolve()
+    .unwrap()
+}
+
+fn add_dns_domain(config: &ResolvedConfig) {
+    let result = Command::new("/usr/bin/resolvectl")
+        .arg("domain")
+        .arg(config.vpn_name.clone())
+        .args(&config.vpn_hosts)
+        .output();
+    match result {
+        Ok(output) => {
+            println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+            println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+        }
+        Err(e) => println!("error add_dns_domain:: {e}"),
+    }
+}
+
+fn call_resolvectl(config: &ResolvedConfig) {
+    let result = Command::new("/usr/bin/resolvectl")
+        .arg("dns")
+        .arg(config.vpn_name.clone())
+        .arg(config.vpn_ip.clone())
+        .output();
+    match result {
+        Ok(output) => {
+            println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+            println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+        }
+        Err(e) => println!("error call_resolvectl: {e}"),
+    }
+}
+
+fn revert_dns_domain(name: String) {
+    let result = Command::new("/usr/bin/resolvectl")
+        .arg("revert")
+        .arg(name)
+        .output();
+    match result {
+        Ok(output) => {
+            println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+            println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+        }
+        Err(e) => println!("error revert_dns_domain: {e}"),
+    }
 }
---
+++ splitway-shared/src/config/mod.rs
@@ -0,0 +1,66 @@
+use std::{fmt::Display, fs::read_to_string};
+
+use serde::{Deserialize, Serialize};
+
+pub fn get_config() -> Result<LocalConfig, ConfigParseError> {
+    let path = config_path();
+    log::info!("Config path: {path}");
+    let confi_str = read_to_string(path).map_err(|e| {
+        log::error!("Error read config file: {e}");
+        ConfigParseError::ConfigNotFound
+    })?;
+    serde_json::from_str::<LocalConfig>(confi_str.as_str()).map_err(|e| {
+        log::error!("Error deserialize: {e}");
+        ConfigParseError::SerializeError
+    })
+}
+pub fn create_empty_config<'a>() -> Result<(), ConfigParseError> {
+    std::fs::create_dir_all(config_folder_path()).map_err(|e| {
+        log::error!("Error create config dir: {e}");
+        ConfigParseError::ConfigNotFound
+    })?;
+    let empty_config = LocalConfig {
+        vpn_name: String::new(),
+        vpn_hosts: Vec::new(),
+    };
+    let empty_config_str = serde_json::to_string(&empty_config).map_err(|e| {
+        log::error!("Error serialize: {e}");
+        ConfigParseError::SerializeError
+    })?;
+    std::fs::write(config_path(), empty_config_str).map_err(|e| {
+        log::error!("Error write config file: {e}");
+        ConfigParseError::ConfigNotFound
+    })?;
+    Ok(())
+}
+
+fn config_path() -> String {
+    config_folder_path() + "/config.json"
+}
+
+fn config_folder_path() -> String {
+    format!("{}/.config/splitway", std::env::var("HOME").unwrap())
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct LocalConfig {
+    pub vpn_name: String,
+    pub vpn_hosts: Vec<String>,
+}
+
+#[derive(Debug)]
+pub enum ConfigParseError {
+    ConfigNotFound,
+    SerializeError,
+    Unresolve,
+}
+
+impl Display for ConfigParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ConfigParseError::ConfigNotFound => write!(f, "Config file not found"),
+            ConfigParseError::SerializeError => write!(f, "Error serialize/deserialize config"),
+            ConfigParseError::Unresolve => write!(f, "Error unresolve config"),
+        }
+    }
+}
